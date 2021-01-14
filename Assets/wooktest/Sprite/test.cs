990d64f3c336cae285d1423dbe32584c2d36c2e9
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-01-14 21:50:27 +0900

    시야

---
+++ Assets/wooktest/Sprite/test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    Camera Camera;
+    void Start()
+    {
+        Camera = GameObject.Find("Camera").GetComponent<Camera>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
