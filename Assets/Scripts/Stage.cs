da697dc3b171ebab2e9351f28e7ee34fc324f63f
LeeJeongHo <aofhd15@gmail.com>
2021-01-14 22:15:35 +0900

    타이틀 씬, 스테이지 선택 화면 구현

---
+++ Assets/Scripts/Stage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Stage
+{
+    //이 클래스는 모노비헤이비어 자식 클래스가 아니라서 오브젝트에 넣어주려면 새 클래스를 만들어서 넣어주세요
+
+    public int Index = 0;
+    public bool isCleared = false;
+    public string SceneName;
+
+}
---
+++ Assets/Scripts/TitleStageButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleStageButton : MonoBehaviour
+{
+    [SerializeField]
+    Text IndexText;
+
+    [SerializeField]
+    Stage StageProperties;
+
+    public void Start()
+    {
+        IndexText = transform.GetChild(0).GetComponent<Text>();
+        IndexText.text = this.StageProperties.Index.ToString();
+    }
+
+    public void DebugProperties()
+    {
+        Debug.Log(this.StageProperties.Index + ", " + this.StageProperties.isCleared + ", " + this.StageProperties.SceneName);
+    }
+
+
+}
