ffc39c7785180febb86730062caa7ece746a0d9d
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-01-15 02:03:13 +0900

    가로등
    Lamp오브젝트 만들고 자식으로 Lamp프리팹등록

---
+++ Assets/Scripts/Lamp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+    public Animator ani;
+    public GameObject Light;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            ani.Play("Lamp_on");
+            Light.SetActive(true);
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Llamp_NPC.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Llamp_NPC : MonoBehaviour
+{
+    public GameObject[] Lamp;
+    public TextMeshPro Tx_text;
+    public GameObject quad;
+    public GameObject Gm_text;
+
+    int FindLamp()
+    {
+        int Lamp_count = 0;
+        GameObject Lamp = GameObject.Find("Lamp");
+        for (int i = 0; i < Lamp.transform.childCount; i++)
+        {
+            Lamp_count++;
+            if (Lamp.transform.GetChild(i).GetComponentInChildren<Lamp>().transform.GetChild(0).gameObject.activeSelf == true)
+                Lamp_count--;         
+        }
+        return Lamp_count;
+    }
+   
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Gm_text.SetActive(true);
+            Tx_text.text = "남은 가로등 갯수: " + FindLamp();
+            float x = Tx_text.preferredWidth;
+            x = (x > 3) ? 3 : x + 0.15f;
+            quad.transform.localScale = new Vector2(x, Tx_text.preferredHeight / 2 + 0.15f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            Gm_text.SetActive(false);
+    }
+
+  
+}
