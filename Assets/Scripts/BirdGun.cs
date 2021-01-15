984701469bf419acc8a5e3678005673200b1d1d6
kimdimu <48170768+kimdimu@users.noreply.github.com>
2021-01-16 02:15:25 +0900

    shoot

---
+++ Assets/Scripts/BirdGun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdGun : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject shootpoint;
+    Vector3 worldMousePos;
+    public float speed;
+    void Update()
+    {
+        worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 dir = worldMousePos - shootpoint.transform.position;
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        shootpoint.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, shootpoint.transform.position, shootpoint.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+    public float speed;
+    private void Start()
+    {
+        rigid.velocity = transform.right * speed;
+    }
+    void Update()
+    {
+        
+    }
+}
