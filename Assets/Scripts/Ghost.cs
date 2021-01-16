a5dc28df07ad087d52947f3a475d28acc78ae6d6
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-01-16 13:50:06 +0900

    망령

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
+    bool isDie = false;
+    Color color;
+    IEnumerator DIe()
+    {
+        SpriteRenderer spr = gameObject.GetComponent<SpriteRenderer>();
+        for (int i = 0; i<10; i++)
+        {
+            color.a = i*0.1f;
+            spr.color = color;
+            yield return new WaitForSeconds(0.1f);
+        }
+        gameObject.SetActive(false);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.tag == "Player" && !isDie)
         {
             if (PlayerController.Instance.Return_isSit == true)
                 return;
             PlayerController.Instance.IncreaseHP(-1);
+        }
+        if(collision.gameObject.tag == "Bullet")
+        {
+            isDie = true;
+            StartCoroutine(DIe());
         }
     }
 }
